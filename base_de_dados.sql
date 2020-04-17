8e13bdfdbebc56009ba9b3b376e37c1fa42019be
barrosronan <61356279+barrosronan@users.noreply.github.com>
2020-04-17 12:17:11 -0300

    Add files via upload

---
+++ base_de_dados.sql
@@ -0,0 +1,23 @@
+create schema empresa default character set utf8;
+use empresa;
+
+create table Funcionario(
+	CodFuncionario int not null primary key AUTO_INCREMENT,
+    Nome varchar(400) not null,
+    DataNascimento datetime not null,
+    Salario numeric(18,2) not null,
+    Atividades varchar(21437) not null
+);
+
+create table FuncionarioFilho(
+	CodFuncionarioFilho int not null primary key auto_increment,
+    CodFuncionario int not null,
+    Nome varchar(400),
+    DataDeNascimento datetime not null,
+    constraint fk_codFuncionario foreign key (CodFuncionario) references Funcionario(CodFuncionario)
+);
+
+SELECT f.CodFuncionario, f.Nome, f.DataNascimento, 
+	(SELECT COUNT(ff.CodFuncionario) FROM FuncionarioFilho ff WHERE f.CodFuncionario = ff.CodFuncionario)
+FROM Funcionario
+WHERE f.nome LIKE CONCAT('%','ro','%');
